8bdf5a92c739050daa759afa32c2c9c2173110e3
francoDavolio <francodavolio12@gmail.com>
2022-11-08 12:39:54 -0300

    acabamos de hacer el modelado de producto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,10 +1,24 @@
+import Producto from '../models/producto'
+
 export const listaProductos = (req, res) => {
   res.send('hola desde el backend en la peticion get')
 }
 
-export const crearProducto = (req, res) => {
+export const crearProducto = async (req, res) => {
+  try {
     console.log(req.body)
     //validar los datos del objetos
     //guardar el objeto en la base de datos
-  res.send('aqui tendria q crear un producto')
+    const productoNuevo = new Producto(req.body)
+    //guarda ese objeto en la base de datos
+    await productoNuevo.save()
+    res.status(201).json({
+      mensaje: 'El producto fue creado correctamente',
+    })
+  } catch (error) {
+    console.log(error)
+    req.status(404).json({
+      mesaje: 'Error al intentar agregar un nuevo producto',
+    })
+  }
 }
