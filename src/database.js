102db90e18fbf23c65b548b663643bf274c033b0
francoDavolio <francodavolio12@gmail.com>
2022-11-08 11:36:46 -0300

    agregue la direccion del localhost comentada

---
+++ src/database.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+const url = 'mongodb://localhost:27017/cafeteria'
+
+mongoose.connect(url)
+
+const connection = mongoose.connection
+
+connection.once('open', () => {
+  console.log('DB conectada')
+})
