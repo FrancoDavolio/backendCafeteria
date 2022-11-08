5d89ff6c5b23f3c0b091ecd26d3ad1a771053b12
francoDavolio <francodavolio12@gmail.com>
2022-11-08 10:42:59 -0300

    agregue el inde,html

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+import path from 'path'
 
 //crear una instancia de express
-const app = express();
+const app = express()
 //configurar un puerto
 app.set('port', process.env.PORT || 4000)
 
-app.listen(app.get('port'),()=>{
-    console.log('Estoy en el puerto ' + app.get('port'));
+app.listen(app.get('port'), () => {
+  console.log('Estoy en el puerto ' + app.get('port'))
 })
 
 //middlewares: son funsiones que se ejecutan antas de llegar a las rutas
+app.use(cors()) //permite peticiones remotas
+//permite recibir y usar formatos json
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+//informacion extra
+app.use(morgan('dev'))
+//cargar un archivo estactico
+app.use(express.static(path.join(__dirname, '../public')))
+console.log(path.join(__dirname, '../public'))
 
 //rutas
+app.get('/prueba', (req, res) => {
+    res.send('hola desde el backend en la peticion get')
+})
